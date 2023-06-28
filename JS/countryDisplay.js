6d6433fe5c3a9339592d9db71fc1cc56a8c9ca03
lawrenceokim <lawrence4okim@gmail.com>
2023-06-28 03:45:42 +0100

    implemented country & neighbour display toggle

--- JS/countryDisplay.js
+++ JS/countryDisplay.js
@@ -1,14 +1,22 @@
 "use strict";
 
-const btn = document.querySelector(".btn-country");
+const btnShowNeighbours = document.querySelector(".btn-show-neighbours");
+const btnFindMe = document.querySelector(".btn-findMe");
+const searchCountryForm = document.querySelector(".search-bar");
+const searchCountryInput = document
+  .querySelector(".search-input")
+  .value.toString()
+  .toLowerCase();
 const container = document.querySelector(".container");
 const neighbourContainer = document.querySelector(".neighbour-container");
 const countryContainer = document.querySelector(".country-container");
 
 ///////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+/////////////////////// FUNCTIONS ///////////////////////////////
 
 const renderError = function (msg) {
-  container.insertAdjacentText("beforeend", msg);
+  container.insertAdjacentText("afterbegin", msg);
   container.style.opacity = 1;
 };
 
@@ -62,71 +70,7 @@ const getJSON = function (url, errorMsg = "Something went wrong") {
   });
 };
 
-/*
-const getCountryData = function (country) {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v2/name/${country}`);
-  request.send();
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
-    renderCountry(data);
-
-    const [...border] = data.borders;
-    if (!border) return;
-
-    border.forEach(borders => {
-      const request2 = new XMLHttpRequest();
-      request2.open('GET', `https://restcountries.com/v2/alpha/${borders}`);
-      request2.send();
-      request2.addEventListener('load', function () {
-        const data2 = JSON.parse(this.responseText);
-        console.log(data2);
-        renderCountry(data2, 'neighbour');
-      });
-    });
-  });
-};
-
-const myLocation = navigator.geolocation.getCurrentPosition(
-  function (position) {
-    console.log(position);
-  },
-  function () {
-    alert('couldnt get your location');
-  }
-);
-
-const getCountryData = function (country) {
-  getJSON(`https://restcountries.com/v2/name/${country}`, 'country not found')
-    .then(data => {
-      renderCountry(data[0]);
-      console.log(data);
-      const neighbour = data[0].borders?.[0];
-      if (!neighbour) throw new Error('No neighbour found!');
-
-      return getJSON(
-        `https://restcountries.com/v2/alpha/${neighbour}`,
-        'country not found'
-      );
-    })
-    .then(data => renderCountry(data, 'neighbour'))
-    .catch(err => {
-      console.error(`${err}⛔⛔⛔`);
-      renderError(
-        `Something went wrong. ${err.message}⛔⛔⛔ Try again later!`
-      );
-    });
-};
-
-btn.addEventListener('click', function () {
-  getCountryData('ukraine');
-});
-
-//coding challenge #1
-*/
-
-const whereAmI = async function (country) {
+const whereAmI = async function () {
   try {
     const pos = await getPosition();
     console.log(pos);
@@ -144,6 +88,38 @@ const whereAmI = async function (country) {
     console.log(res);
     const data = await res.json();
     console.log(data);
+
+    renderCountry(data[0]);
+    countryContainer.classList.toggle("show");
+    const [...border] = data[0].borders;
+    if (!border) throw new Error("Neighbouring Country not found");
+    console.log(border);
+    border.forEach(async (borders) => {
+      const res2 = await fetch(`https://restcountries.com/v2/alpha/${borders}`);
+      if (!res.ok) throw new Error("Problem getting neighbour");
+      const data2 = await res2.json();
+      console.log(data2);
+
+      renderNeighbour(data2);
+    });
+  } catch (err) {
+    console.error(`${err}⛔`);
+    renderError(`${err.message}⛔ Try reloading!`);
+  }
+};
+whereAmI();
+
+searchCountryForm.addEventListener("submit", async function (e) {
+  container.innerHTML = "";
+  e.preventDefault();
+  try {
+    const res = await fetch(
+      `https://restcountries.com/v2/name/${searchCountryInput}`
+    );
+    if (!res.ok) throw new Error("Problem getting country");
+    console.log(res);
+    const data = await res.json();
+    console.log(data);
     renderCountry(data[0]);
     const [...border] = data[0].borders;
     if (!border) throw new Error("Neighbouring Country not found");
@@ -153,17 +129,19 @@ const whereAmI = async function (country) {
       if (!res.ok) throw new Error("Problem getting neighbour");
       const data2 = await res2.json();
       console.log(data2);
-      return renderNeighbour(data2);
+      renderNeighbour(data2);
     });
   } catch (err) {
     console.error(`${err}⛔`);
     renderError(`${err.message}⛔ Try reloading!`);
   }
-};
-whereAmI();
+});
+////////////////////////////////////////////////////////////////////////
+/////////////////////// EVENT LISTENERS ///////////////////////////////
+btnFindMe.addEventListener("click", function () {
+  countryContainer.classList.toggle("show");
+});
 
-Promise.any([
-  Promise.resolve("success"),
-  Promise.reject("ERROR"),
-  Promise.resolve("another success"),
-]).then((res) => console.log(res));
+btnShowNeighbours.addEventListener("click", function () {
+  neighbourContainer.classList.toggle("show");
+});
